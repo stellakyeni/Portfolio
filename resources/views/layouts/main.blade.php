5ca5ab1493f5ddcc9b5d0c4916a149ffc730052d
stella kyeni <stellakyeni55@gmail.com>
2024-01-11 22:45:03 +0300

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with JohnDoe landing page.">
+    <meta name="author" content="Devcrud">
+    <title>Stella K. Kimanzi portfolio</title>
+    <!-- font icons -->
+    <link rel="stylesheet" href="/assets/vendors/themify-icons/css/themify-icons.css">
+    <!-- Bootstrap + JohnDoe main styles -->
+    <link rel="stylesheet" href="/assets/css/johndoe.css">
+
+
+
+    <!-- core  -->
+    <script src="/assets/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="/assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+
+    <!-- bootstrap 3 affix -->
+    <script src="/assets/vendors/bootstrap/bootstrap.affix.js"></script>
+
+    <!-- Isotope  -->
+    <script src="/assets/vendors/isotope/isotope.pkgd.js"></script>
+
+    <!-- Google mpas -->
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtme10pzgKSPeJVJrG1O3tjR6lk98o4w8&callback=initMap"></script>
+
+    <!-- JohnDoe js -->
+    <script src="/assets/js/johndoe.js"></script>
+</head>
+
+<body>
+
+
+</body>
+
+
+</html>
---
+++ resources/views/partials/news.blade.php
@@ -0,0 +1,34 @@
+<section class="section" id="blog">
+    <div class="container">
+        <h2 class="mb-5">Latest <span class="text-danger">News</span></h2>
+        <div class="row">
+            <div class="blog-card">
+                <div class="img-holder">
+                    <img src="/assets/imgs/blog1.jpg"
+                        alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page">
+                </div>
+                <div class="content-holder">
+                    <h6 class="title">Consectetur adipisicing elit</h6>
+
+                    <p class="post-details">
+                        <a href="#">By: Admin</a>
+                        <a href="#"><i class="ti-heart text-danger"></i> Stella</a>
+                        <a href="#"><i class="ti-comment"></i> news</a>
+                    </p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nesciunt qui sit velit
+                        delectus voluptates, repellat ipsum culpa id deleniti. Rerum debitis facilis accusantium
+                        neque numquam mollitia modi quasi distinctio.</p>
+
+                    <p><b>Necessitatibus nihil impedit! Ex nisi eveniet, dolor aliquid consequuntur repudiandae.</b>
+                    </p>
+                    <p>Magnam in repellat enim harum omnis aperiam! Explicabo illo, commodi, dolor blanditiis
+                        cupiditate harum nisi vero accusamus laudantium voluptatibus dolores quae obcaecati.</p>
+
+                    <a href="#" class="read-more">Read more <i class="ti-angle-double-right"></i></a>
+                </div>
+            </div><!-- end of blog wrapper -->
+
+        </div>
+    </div>
+</section>
